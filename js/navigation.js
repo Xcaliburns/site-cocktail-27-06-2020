decf563756b2dff990b6939ada648026e865bd16
Xcaliburns <davidabruzzogames@gmail.com>
2020-06-22 10:27:05 +0200

    Initial commit

---
+++ js/navigation.js
@@ -0,0 +1,32 @@
+'<div class="container">
+            <div class="row"> 
+               <div class="col-lg-12 ">
+         <nav class="navbar navbar-expand-md navbar-fixed-top navbar-#0b132b  main-nav">
+                <ul class="nav navbar-nav">
+           <li class="nav-item cadreMenu"><button class="btn btn-default ">
+        <a class="nav-link active" href="index.html"><svg fill="#000000" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="48px" height="48px"><path d="M 12 2.0996094 L 1 12 L 4 12 L 4 21 L 10 21 L 10 15 L 14 15 L 14 21 L 20 21 L 20 12 L 23 12 L 12 2.0996094 z"/></svg></a></button>
+      </li>     
+         
+          <li class="nav-item cadreMenu dropdown">
+           <button class="btn btn-default dropdown-toggle" data-toggle ="dropdown"> Themes<span class="caret"></span></button>
+            
+            <ul class="dropdown-menu ">
+              <li class="nav-item cadreMenu ">
+
+                <a href="#"><p class="menuTexte">Par alcool</p></a></li>
+              <li class="nav-item cadreMenu"><a href="#"><p class="menuTexte">Sans alcool<p></a></li>
+              <li class="nav-item cadreMenu"><a href="#"><p class="menuTexte">Classiques</p></a></li>
+              <li class="nav-item cadreMenu"><a href="#"><p class="menuTexte">Autres</p> </a></li>
+            </ul>
+          </li>
+          <li class="nav-item cadreMenu ">
+        <a class="nav-link active menuTexte" href="#">A-Z</a>
+        
+         
+        </ul>
+         
+</div>
+</nav>
+</div>
+</div>
+</div>'
